0df1baa126156c67c6274169d854796bb4f988fc
Emily <cas20037@byui.edu>
2023-02-09 16:54:22 -0700

    moved swagger after ./routes

--- server.js
+++ server.js
@@ -5,12 +5,7 @@ const mongodb = require('./db/connect');
 const port = process.env.PORT || 8080;
 const app = express();
 
-// express setup
-const swaggerUi = require('swagger-ui-express');
-const swaggerDocument = require('./swagger.json');
-
 app
-.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument)) // express setup
   .use(bodyParser.json())
   .use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
@@ -20,6 +15,13 @@ app
     next();
   })
   .use('/', require('./routes'));
+
+// express setup
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument)) // express setup
+
 
 // handling errors - catch all errors
 process.on('uncaughtException', (err, origin) => {
