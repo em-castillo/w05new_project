92215fe43ffb2bd55b37b8381d122ab8d4480812
Emily <cas20037@byui.edu>
2023-02-18 23:05:48 -0700

    render

--- swagger.js
+++ swagger.js
@@ -7,8 +7,8 @@ const doc = {
     description: 'books',
   },
   // match port from render this time including https
-  //host: 'childrens-books.onrender.com',
-  host: 'localhost:8080',
+  host: 'childrens-books.onrender.com',
+  // host: 'localhost:8080',
   schemes: ['http', 'https'],
 };
 
